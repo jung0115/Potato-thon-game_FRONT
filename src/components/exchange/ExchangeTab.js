775696091147ec88270c39c472e34aeefff8223e
jung0115 <jungim7490@gmail.com>
2023-10-01 01:59:05 +0900

    [FEAT] #04 거래소 정리 중

--- src/components/exchange/ExchangeTab.js
+++ src/components/exchange/ExchangeTab.js
@@ -1,10 +1,21 @@
 // 거래소 탭
 import React from "react";
+import { useEffect, useState } from "react";
+
+import styled from "styled-components";
+import palette from "../../styles/colorPalatte";
 
 const ExchangeTab = () => {
   return(
-    <div style={{color: '#ffffff'}}>거래소</div>
+    <Container>
+      
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: block;
+  background-color: ${palette.bg_color};
+`;
 
 export default ExchangeTab;
