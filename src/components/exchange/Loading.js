782edbbfff3c4aa98a5f598b0817a72d2f5e5c18
jung0115 <jungim7490@gmail.com>
2023-11-08 00:49:21 +0900

    [UPDATE] #05 - 도움말 아이콘 크기 수정

---
+++ src/components/exchange/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import styled from "styled-components";
+import palette from "../../styles/colorPalatte";
+
+const Question = () => {
+  return(
+    <Container></Container>
+  );
+}
+
+const Container = styled.div`
+  display: block;
+  background-color: ${palette.bg_color};
+  margin-top: 28px;  
+`;
